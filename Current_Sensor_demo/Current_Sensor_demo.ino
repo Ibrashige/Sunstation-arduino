259bdb6d5be3b3e901ee219e5b48379a5164918a
AbdulazizAlmoallim <aziz.almoallim@gmail.com>
2020-02-14 12:50:04 -0500

    Updated

--- Current_Sensor_demo/Current_Sensor_demo.ino
+++ Current_Sensor_demo/Current_Sensor_demo.ino
@@ -1,8 +1,8 @@
 #include <SPI.h>
 #include <SD.h>
 
-//int offset = 130;
-//const float vpp = 0.0048828125;
+int offset = 510;
+const float vpp = 0.0048828125;
 int points;
 int interval;
 float voltage;
@@ -32,9 +32,9 @@ void setup() {
 
 void loop() {
   interval = 1; //interval to write data
-  points = analogRead(A0) //- offset;
-  voltage = points //* vpp;
-  amperage = voltage / 0.02055;
+  points = analogRead(A0)- offset;
+  voltage = points * vpp;
+  amperage = voltage / 0.185;
   Serial.println(amperage);
   
   write_data(amperage);  //write data
