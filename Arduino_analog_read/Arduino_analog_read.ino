bfca33e520238628939a20f1fea241f6880205e8
Ibrashige <61058805+Ibrashige@users.noreply.github.com>
2020-03-24 14:27:20 -0400

    Add files via upload

---
+++ Arduino_analog_read/Arduino_analog_read.ino
@@ -0,0 +1,40 @@
+// Print battery voltage
+// to 16x2 LCD via I2C
+// with Voltage Divider (2x 10K resistor)
+/*
+  Resistors are aligned in series.
+  One end goes to Battery - and also to Arduino GND
+  The other goes to Battery + and also to Arduino Vin
+  The middle (connection between two resistors) goes to Arduino A0
+*/
+
+#include <Wire.h>
+#define led_pin 13
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(led_pin, OUTPUT);
+  digitalWrite(led_pin, LOW);
+}
+
+void loop()
+{
+  printVolts();
+}
+ 
+ void printVolts()
+{
+  int sensorValue = analogRead(A0); //read the A0 pin value
+  float voltage = sensorValue * (5.00 / 1023.00) * 2; //convert the value to a true voltage.
+  Serial.print("voltage = ");
+  Serial.print(voltage); 
+  Serial.println("V");
+  
+  if (voltage < 3.50) //set the voltage considered low battery here
+  {
+    digitalWrite(led_pin, HIGH);
+  }
+
+  delay(5000);
+}
